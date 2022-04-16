68a0b909e39adc5899e79a495703c11503f01a4b
Ana Maria Mitrea <anamitrea27@gmail.com>
2022-04-16 14:26:50 +0300

    Update Shopping List Div

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,107 +1,127 @@
-const taskInput = document.querySelector(".task-input input"),
+const itemInput = document.querySelector(".item-input input"),
 filters = document.querySelectorAll(".filters span"),
 clearAll = document.querySelector(".clear-btn"),
-taskBox = document.querySelector(".task-box");
+itemBox = document.querySelector(".item-box");
 
-let editId,
-isEditTask = false,
-todos = JSON.parse(localStorage.getItem("todo-list"));
+let editId;
+let isEditItem = false;
 
-/* activate button fot Clear All */
+/*
+*  Parsing item list in order to convert text into js object.
+*  Data is saved until page is closed.
+*/
+let products = JSON.parse(localStorage.getItem("products-list"));
+
+/* activate button for Clear All */
 filters.forEach(btn => {
     btn.addEventListener("click", () => {
         document.querySelector("span.active").classList.remove("active");
         btn.classList.add("active");
-        showTodo(btn.id);
+        showShoppingList(btn.id);
     });
 });
 
-function showTodo(filter) {
-    let liTag = "";
-    if(todos) {
-        todos.forEach((todo, id) => {
-            let completed = todo.status == "completed" ? "checked" : "";
-            if(filter == todo.status || filter == "all") {
-                liTag += `<li class="task">
+/* Display Shopping List Items */
+function showShoppingList(filter) {
+    let listTag = "";
+    if(products) {
+        products.forEach((product, id) => {
+            let completed = product.status === "completed" ? "checked" : "";
+            if(filter === "all") {
+                /* Displaying Item-box */
+                listTag += `<li class="task">
                             <label for="${id}">
-                                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
-                                <p class="${completed}">${todo.name}</p>
+                                <input type="checkbox" onclick="updateStatus(this)" id="${id}" ${completed} >
+                                <p class="${completed}" >${product.name}</p>
                             </label>
-                            <div class="settings">
+                            
+                            <div class="configs">
                                 <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-                                <ul class="task-menu">
-                                    <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
-                                    <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                                <ul class="item-menu">
+                                    <li onclick='editItem(${id}, "${product.name}")' ><i class="uil uil-pen" style="color:#5a9f7d"></i>Edit</li>
+                                    <li onclick='deleteItem(${id}, "${filter}")' ><i class="uil uil-trash" style="color:#5a9f7d"></i>Delete</li>
                                 </ul>
                             </div>
-                        </li>`;
+                            </li>`;
             }
         });
     }
-    taskBox.innerHTML = liTag || `<span style="color: lightgrey">You don't have any items yet.</span>`;
-    let checkTask = taskBox.querySelectorAll(".task");
-    !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
-    taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
-}
-showTodo("all");
 
+    /* Display the entire list of items or "Notification text" */
+    itemBox.innerHTML = listTag || `<span style="color: lightgrey">You don't have any items yet.</span>`;
+
+    let checkItem = itemBox.querySelectorAll(".task");
+    !checkItem.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
+    itemBox.offsetHeight >= 300 ? itemBox.classList.add("overflow") : itemBox.classList.remove("overflow");
+}
+showShoppingList("all");
+
+/* Edit or Delete Menu buttons */
 function showMenu(selectedTask) {
     let menuDiv = selectedTask.parentElement.lastElementChild;
     menuDiv.classList.add("show");
     document.addEventListener("click", e => {
-        if(e.target.tagName != "I" || e.target != selectedTask) {
+        if(e.target.tagName !== "I" || e.target !== selectedTask) {
             menuDiv.classList.remove("show");
         }
     });
 }
 
+/* Pending or Completed Status */
 function updateStatus(selectedTask) {
     let taskName = selectedTask.parentElement.lastElementChild;
     if(selectedTask.checked) {
         taskName.classList.add("checked");
-        todos[selectedTask.id].status = "completed";
+        products[selectedTask.id].status = "completed";
     } else {
         taskName.classList.remove("checked");
-        todos[selectedTask.id].status = "pending";
+        products[selectedTask.id].status = "pending";
     }
-    localStorage.setItem("todo-list", JSON.stringify(todos))
+    localStorage.setItem("products-list", JSON.stringify(products))
 }
 
-function editTask(taskId, textName) {
+/* Edits name of product */
+function editItem(taskId, textName) {
     editId = taskId;
-    isEditTask = true;
-    taskInput.value = textName;
-    taskInput.focus();
-    taskInput.classList.add("active");
+    isEditItem = true;
+    itemInput.value = textName;
+    itemInput.focus();
+    itemInput.classList.add("active");
 }
 
-function deleteTask(deleteId, filter) {
-    isEditTask = false;
-    todos.splice(deleteId, 1);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo(filter);
+/* Deletes Item from Shopping List */
+function deleteItem(deleteId, filter) {
+    isEditItem = false;
+    products.splice(deleteId, 1);
+    localStorage.setItem("products-list", JSON.stringify(products));
+    showShoppingList(filter);
 }
 
+/* Deletes all items when clearAll btn is clicked */
 clearAll.addEventListener("click", () => {
-    isEditTask = false;
-    todos.splice(0, todos.length);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo()
+    isEditItem = false;
+    products.splice(0, products.length);
+    localStorage.setItem("products-list", JSON.stringify(products));
+    showShoppingList()
 });
 
-taskInput.addEventListener("keyup", e => {
-    let userTask = taskInput.value.trim();
-    if(e.key == "Enter" && userTask) {
-        if(!isEditTask) {
-            todos = !todos ? [] : todos;
-            let taskInfo = {name: userTask, status: "pending"};
-            todos.push(taskInfo);
+/*
+* Prevents User to add a null value into shopping List
+*/
+itemInput.addEventListener("keyup", e => {
+    let userItem = itemInput.value.trim();
+    if(e.key === "Enter" && userItem) {
+        if(!isEditItem) {
+            products = !products ? [] : products;
+            let itemInfo = {name: userItem, status: "pending"};
+            products.push(itemInfo);
         } else {
-            isEditTask = false;
-            todos[editId].name = userTask;
+            isEditItem = false;
+            products[editId].name = userItem;
         }
-        taskInput.value = "";
-        localStorage.setItem("todo-list", JSON.stringify(todos));
-        showTodo(document.querySelector("span.active").id);
+
+        itemInput.value = "";
+        localStorage.setItem("products-list", JSON.stringify(products));
+        showShoppingList(document.querySelector("span.active").id);
     }
 });
