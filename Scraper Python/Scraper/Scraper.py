80b1fb8f92ad3375ecdbc403a4a894076b35c633
stefanarnautu <stefanarnautu01@gmail.com>
2022-05-19 20:09:20 +0300

    Script

--- Scraper Python/Scraper/Scraper.py
+++ Scraper Python/Scraper/Scraper.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://world.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains' \
-      '&tag_0=beverages&tagtype_1=categories&tag_contains_1=contains&tag_1=coffee_drinks&page=1&json=true'
+      '&tag_0=beverages&tagtype_1=categories&tag_contains_1=contains&tag_1=carbonated_drinks&page=4&json=true'
 
 r = requests.get(url)
 
@@ -15,7 +15,7 @@ script = soup.get_text()
 data = json.loads(script)
 products = data['products']
 
-with open('testare.csv', 'w', encoding='utf-8') as f:
+with open('carbonated_drinks_4.csv', 'w', encoding='utf-8') as f:
     # header
     header = ['code',
               'image_url',
@@ -189,93 +189,134 @@ with open('testare.csv', 'w', encoding='utf-8') as f:
         # nutriments ( 36 de campuri )
         if "nutriments" in product:
             nutriments = {
-                "energy_kj_value": "NULL",
-                "energy_kj_unit": "NULL",
-                "energy_kcal_value": "NULL",
+                "10energy_kj_value": "NULL",
+                "11energy_kj_unit": "NULL",
+                "12energy_kcal_value": "NULL",
+                "13energy_kcal_unit": "NULL",
+                "14fat_value": "NULL",
+                "15fat_unit": "NULL",
+                "16saturated_fat_value": "NULL",
+                "17saturated_fat_unit": "NULL",
+                "18carbohydrates_value": "NULL",
+                "19carbohydrates_unit": "NULL",
 
-                # de completat
+                "20sugars_value": "NULL",
+                "21sugars_unit": "NULL",
+                "22fiber_value": "NULL",
+                "23fiber_unit": "NULL",
+                "24proteins_value": "NULL",
+                "25proteins_unit": "NULL",
+                "26salt_value": "NULL",
+                "27salt_unit": "NULL",
+                "28sodium_value": "NULL",
+                "29sodium_unit": "NULL",
+
+                "30energy_value": "NULL",
+                "31energy_unit": "NULL",
+                "32vitamin_c_value": "NULL",
+                "33vitamin_c_unit": "NULL",
+                "34vitamin_b6_value": "NULL",
+                "35vitamin_b6_unit": "NULL",
+                "36vitamin_b12_value": "NULL",
+                "37vitamin_b12_unit": "NULL",
+                "38potassium_value": "NULL",
+                "39potassium_unit": "NULL",
+
+                "40calcium_value": "NULL",
+                "41calcium_unit": "NULL",
+                "42caffeine_value": "NULL",
+                "43caffeine_unit": "NULL",
+                "44taurine_value": "NULL",
+                "45taurine_unit": "NULL"
             }
+            #  "food_groups_tags": "NULL",
+            #  "nutriscore_grade": "NULL"
+
 
             product_dictionary = product['nutriments']
             pairs = product_dictionary.items()
 
             # fiecare key, value
             for key, value in pairs:
-                if "energy_kj_value" == key:
-                    nutriments["energy_kj_value"] = value
-                elif "energy_kj_unit" == key:
-                    nutriments["energy_kj_unit"] = value
-                elif "energy_kcal_value" == key:
-                    nutriments["energy_kcal_value"] = value
-                elif "energy_kcal_unit" == key:
-                    strings.append(value)
+                if "energy-kj_value" == key:
+                    nutriments["10energy_kj_value"] = value
+                elif "energy-kj_unit" == key:
+                    nutriments["11energy_kj_unit"] = value
+                elif "energy-kcal_value" == key:
+                    nutriments["12energy_kcal_value"] = value
+                elif "energy-kcal_unit" == key:
+                    nutriments["13energy_kcal_unit"] = value
                 elif "fat_value" == key:
-                    strings.append(value)
+                    nutriments["14fat_value"] = value
                 elif "fat_unit" == key:
-                    strings.append(value)
-                elif "saturated_fat_value" == key:
-                    strings.append(value)
-                elif "saturated_fat_unit" == key:
-                    strings.append(value)
+                    nutriments["15fat_unit"] = value
+                elif "saturated-fat_value" == key:
+                    nutriments["16saturated_fat_value"] = value
+                elif "saturated-fat_unit" == key:
+                    nutriments["17saturated_fat_unit"] = value
                 elif "carbohydrates_value" == key:
-                    strings.append(value)
+                    nutriments["18carbohydrates_value"] = value
                 elif "carbohydrates_unit" == key:
-                    strings.append(value)
+                    nutriments["19carbohydrates_unit"] = value
                 elif "sugars_value" == key:
-                    strings.append(value)
+                    nutriments["20sugars_value"] = value
                 elif "sugars_unit" == key:
-                    strings.append(value)
+                    nutriments["21sugars_unit"] = value
                 elif "fiber_value" == key:
-                    strings.append(value)
+                    nutriments["22fiber_value"] = value
                 elif "fiber_unit" == key:
-                    strings.append(value)
+                    nutriments["23fiber_unit"] = value
                 elif "proteins_value" == key:
-                    strings.append(value)
+                    nutriments["24proteins_value"] = value
                 elif "proteins_unit" == key:
-                    strings.append(value)
+                    nutriments["25proteins_unit"] = value
                 elif "salt_value" == key:
-                    strings.append(value)
+                    nutriments["26salt_value"] = value
                 elif "salt_unit" == key:
-                    strings.append(value)
+                    nutriments["27salt_unit"] = value
                 elif "sodium_value" == key:
-                    strings.append(value)
+                    nutriments["28sodium_value"] = value
                 elif "sodium_unit" == key:
-                    strings.append(value)
+                    nutriments["29sodium_unit"] = value
                 elif "energy_value" == key:
-                    strings.append(value)
+                    nutriments["30energy_value"] = value
                 elif "energy_unit" == key:
-                    strings.append(value)
-                elif "vitamin_c_value" == key:
-                    strings.append(value)
-                elif "vitamin_c_unit" == key:
-                    strings.append(value)
-                elif "vitamin_b6_value" == key:
-                    strings.append(value)
-                elif "vitamin_b6_unit" == key:
-                    strings.append(value)
-                elif "vitamin_b12_value" == key:
-                    strings.append(value)
-                elif "vitamin_b12_unit" == key:
-                    strings.append(value)
+                    nutriments["31energy_unit"] = value
+                elif "vitamin-c_value" == key:
+                    nutriments["32vitamin_c_value"] = value
+                elif "vitamin-c_unit" == key:
+                    nutriments["33vitamin_c_unit"] = value
+                elif "vitamin-b6_value" == key:
+                    nutriments["34vitamin_b6_value"] = value
+                elif "vitamin-b6_unit" == key:
+                    nutriments["35vitamin_b6_unit"] = value
+                elif "vitamin-b12_value" == key:
+                    nutriments["36vitamin_b12_value"] = value
+                elif "vitamin-b12_unit" == key:
+                    nutriments["37vitamin_b12_unit"] = value
                 elif "potassium_value" == key:
-                    strings.append(value)
+                    nutriments["38potassium_value"] = value
                 elif "potassium_unit" == key:
-                    strings.append(value)
+                    nutriments["39potassium_unit"] = value
                 elif "calcium_value" == key:
-                    strings.append(value)
+                    nutriments["40calcium_value"] = value
                 elif "calcium_unit" == key:
-                    strings.append(value)
+                    nutriments["41calcium_unit"] = value
                 elif "caffeine_value" == key:
-                    strings.append(value)
+                    nutriments["42caffeine_value"] = value
                 elif "caffeine_unit" == key:
-                    strings.append(value)
+                    nutriments["43caffeine_unit"] = value
                 elif "taurine_value" == key:
-                    strings.append(value)
+                    nutriments["44taurine_value"] = value
                 elif "taurine_unit" == key:
-                    strings.append(value)
+                    nutriments["45taurine_unit"] = value
 
             # trebuie facut append la string folosind valorile din dictionarul nutriments facut anterior in for
             # de continuat...
+
+            for key in nutriments:
+                strings.append(nutriments[key])
+
         else:
             for x in range(1, 36):
                 # cazul in care 'nutriments' nu se gaseste in tot json-ul
