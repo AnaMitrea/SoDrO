c65d636f513abdbb564554825cb62413ca274c37
Ana Maria Mitrea <anamitrea27@gmail.com>
2022-05-22 19:40:31 +0300

    Moved folders

--- login/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$host = 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com';
-$db = 'd402r65h69mmvh';
-$user = 'dkhlzozpfyofur';
-$password = '9eb1ce3e348d0694be81a27b5f4ffc899b6fa83dbb42d2c4cb31c627fb7c3ce5';
-
-?>
